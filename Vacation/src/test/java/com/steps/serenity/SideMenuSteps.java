8784d09fd2955d0d5a626d4febdab7777c108b42
angelabaciu <angelabaciu@evozon.com>
2015-11-17 15:26:22 +0200

    SideMenuPage

--- Vacation/src/test/java/com/steps/serenity/SideMenuSteps.java
+++ Vacation/src/test/java/com/steps/serenity/SideMenuSteps.java
@@ -2,32 +2,49 @@ package com.steps.serenity;
 
 import com.pages.SideMenuPage;
 
-public class SideMenuSteps {
+import helpers.Constants;
+import net.thucydides.core.annotations.Step;
+import net.thucydides.core.annotations.Steps;
+import net.thucydides.core.steps.ScenarioSteps;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.hasItem;
+
+public class SideMenuSteps extends ScenarioSteps {
 	SideMenuPage sideMenuPage;
 	
+	
+	@Step
 	public void enterInbox() {
 		sideMenuPage.clickOnInbox();
 	}
+	  
+	  @Step
 	public void enterNewRequest() {
 		sideMenuPage.clickOnNewRequest();
 	}
+	  @Step
 	public void enterMyRequest() {
 		sideMenuPage.clickOnMyRequest();
 	}
+	 @Step
 	public void enterMyFreeDays() {
 		sideMenuPage.clickOnMyFreeDays();
 	}
+	  @Step
 	public void enterFreeDaysHistory() {
 		sideMenuPage.clickOnFreeDaysHistory();
 	}
+	  @Step
 	public void enterVacatioTracker() {
 		sideMenuPage.clickOnVacationTracker();
 	}
-	
+	  @Step
 	public void enterViewVacations() {
 		sideMenuPage.clickOnViewVacations();
 	}
+	  @Step
 	public void enterVacationReport() {
 		sideMenuPage.clickOnVacationReport();
 	}
