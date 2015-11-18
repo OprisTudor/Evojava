812fcfb35fae8f8ef965d9c6995716b707bfa175
angelabaciu <angelabaciu@evozon.com>
2015-11-18 18:04:02 +0200

    Apply view vacations

---
+++ Vacation/src/test/java/com/pages/ViewVacantionsPage.java
@@ -0,0 +1,87 @@
+package com.pages;
+
+import java.util.List;
+
+import net.serenitybdd.core.annotations.findby.FindBy;
+import net.serenitybdd.core.pages.WebElementFacade;
+import net.thucydides.core.annotations.DefaultUrl;
+import net.thucydides.core.pages.PageObject;
+
+@DefaultUrl("http://172.22.4.88:9090/")
+public class ViewVacantionsPage extends PageObject {
+
+	@FindBy(css = "a[href*='_menuItem=view-vacations']")
+	private WebElementFacade viewVacations;
+
+	@FindBy(css = "div[class='aui-column column-three aui-column-first '] label")
+	private List<WebElementFacade> vacationTypeColumn;
+
+	@FindBy(css = "div[class='aui-column column-three column-center '] label")
+	private List<WebElementFacade> daysNumberColumn;
+
+	@FindBy(css = "div[class='aui-column column-three column-center aui-column-last'] label")
+	private List<WebElementFacade> vacationStatusColumn;
+
+	@FindBy(id = "_evovacation_WAR_EvoVacationportlet_inactiveUsersCheckbox")
+	private WebElementFacade showInactiveUsers;
+
+	@FindBy(id = "_evovacation_WAR_EvoVacationportlet_viewVacationsLastName")
+	private WebElementFacade lastName;
+
+	@FindBy(id = "_evovacation_WAR_EvoVacationportlet_viewVacationsFirstName")
+	private WebElementFacade firstName;
+
+	@FindBy(id = "_evovacation_WAR_EvoVacationportlet_applyButton")
+	private WebElementFacade apply;
+
+	public void clickOnViewVacations() {
+		viewVacations.click();
+	}
+
+	public void clickOnVacationType(String name) {
+		for (WebElementFacade item : vacationTypeColumn) {
+			if (item.getText().contains(name)) {
+				System.out.println("Am dat click");
+				item.click();
+			}
+		}
+	}
+
+	public void clickOnDaysNumber(String name) {
+		for (WebElementFacade item : daysNumberColumn) {
+			System.out.println(item.getText());
+			if (item.getText().contains(name)) {
+				System.out.println("Am dat click");
+				item.click();
+			}
+		}
+	}
+
+	public void clickVacationStatus(String name) {
+		for (WebElementFacade item : vacationStatusColumn) {
+			System.out.println(item.getText());
+			if (item.getText().contains(name)) {
+				System.out.println("Am dat click");
+				item.click();
+			}
+		}
+
+	}
+
+	public void clickOnShowInactiveUsers() {
+		showInactiveUsers.click();
+	}
+
+	public void enterOnLastName(String name) {
+		lastName.type(name);
+	}
+
+	public void enterOnFirstName(String name) {
+		firstName.type(name);
+
+	}
+
+	public void clickOnApply() {
+		apply.click();
+	}
+}
---
+++ Vacation/src/test/java/com/steps/serenity/ViewVacantionsSteps.java
@@ -0,0 +1,44 @@
+package com.steps.serenity;
+
+import com.pages.ViewVacantionsPage;
+
+import net.thucydides.core.annotations.Step;
+
+public class ViewVacantionsSteps {
+	ViewVacantionsPage viewVacationsPage;
+
+	@Step
+	public void clickOnVacationType(String name) {
+		viewVacationsPage.clickOnVacationType(name);
+	}
+
+	@Step
+	public void clickOnDaysNumber(String name) {
+		viewVacationsPage.clickOnDaysNumber(name);
+	}
+
+	@Step
+	public void clickOnVacationStatus(String name) {
+		viewVacationsPage.clickVacationStatus(name);
+	}
+
+	@Step
+	public void clickShowInactiveUsers() {
+		viewVacationsPage.clickOnShowInactiveUsers();
+	}
+
+	@Step
+	public void enterLastName(String name) {
+		viewVacationsPage.enterOnLastName(name);
+	}
+
+	@Step
+	public void enterFirstName(String name) {
+		viewVacationsPage.enterOnFirstName(name);
+	}
+	
+	@Step
+	public void clickOnApply() {
+		viewVacationsPage.clickOnApply();
+	}
+}
