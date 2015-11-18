f4fb9fdff863048d045ba72c5a3509769877f87e
opris.tudor <tudoropris@EVZPC023.evointern>
2015-11-18 16:30:18 +0200

    Check that row contains terms

--- Vacation/src/test/java/com/steps/serenity/MyRequestSteps.java
+++ Vacation/src/test/java/com/steps/serenity/MyRequestSteps.java
@@ -1,6 +1,7 @@
 package com.steps.serenity;
 
 import com.pages.LoginPage;
+import static org.junit.Assert.*;
 import com.pages.MyRequestPage;
 
 import helpers.Constants;
@@ -11,22 +12,44 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.hasItem;
 
+import java.util.List;
+
 public class MyRequestSteps extends ScenarioSteps {
 
-    MyRequestPage myRequestPage;
+	MyRequestPage myRequestPage;
 
-    @Step
-    public void selectHolidayCheckbox() {
-    	myRequestPage.inputHoliday();
-    }
-    
-    public void applyFilters(){
-    	myRequestPage.clickApplyButton();
-    }
-    
-    @Step
-    public void shouldMatchFilterData(String definition) {
-        assertThat(myRequestPage.getFilterData(), hasItem(containsString(definition)));
-    }
-    
+	@Step
+	public void selectHolidayCheckbox() {
+		myRequestPage.inputHoliday();
+	}
+
+	public void applyFilters() {
+		myRequestPage.clickApplyButton();
+	}
+
+	@Step
+	public void verifyThatTypeIsCorrect(String type) {
+		// myRequestPage.verifyThatTypeIsCorrect(type);
+		for (int i = 0; i < myRequestPage.getNumberOfPages(); i++) {
+			myRequestPage.verifyThatTypeIsCorrect(type);
+			goToNextPage();
+			// myRequestPage.verifyThatTypeIsCorrect(type);
+		}
+	}
+
+	@Step
+	public void selectSpecialVacation() {
+		myRequestPage.clickSpecialVacationCheckbox();
+	}
+
+	@Step
+	public void checkThatRowContainsTerms(String... terms) {
+		myRequestPage.checkThatRowContainsTerms(terms);
+	}
+
+	@Step
+	public void goToNextPage() {
+		myRequestPage.clickNextPageButton();
+	}
+
 }
